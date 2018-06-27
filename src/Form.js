cde1d96630fed183d4e9ea06b8977cb40039be87
Leo Veres <leoveres@gmail.com>
2018-06-27 16:12:32 -0400

    final commit

--- src/Form.js
+++ src/Form.js
@@ -7,15 +7,40 @@ import {required, nonEmpty, lenFive, onlyNum} from './validators';
 export class TestForm extends React.Component {
     onSubmit(values){
         console.log(values)
-    }
-
+        return fetch('https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
+            method: 'POST',
+            body: JSON.stringify(values),
+        })
+        .then(res=>{
+            if(!res.ok){
+                throw new SubmissionError({
+                 _error: 'Error submitting message'
+                })
+        }
+    })
+    // .then(res => {console.log(res)})
+}
 
     render(){
+        let successMessage;
+        if (this.props.submitSucceeded){
+            successMessage = (
+                <div className= "message">Success!</div>
+            )
+        }
+        let errorMessage;
+        if (this.props.error){
+            errorMessage = (
+                <div className= "message">{this.props.error}</div>
+            )
+        }
         return(
         <div className='delivery-form'>
             <form
                 onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
             >
+                {successMessage}
+                {errorMessage}
                 <Field
                     name= 'trackingNumber'
                     type= 'text'
@@ -25,7 +50,7 @@ export class TestForm extends React.Component {
                 />
                 <div className='form-input'>
                     <label for='issue'>issue</label>
-                        <Field name="issue" component="select">
+                        <Field name="issue" component="select" validate= {required} >
                             <option value="not-delivered">My delivery hasn't arrived</option>
                             <option value="wrong-item">The wrong item was delivered</option>
                             <option value="missing-part">Part of my order was missing</option>
@@ -48,6 +73,8 @@ export class TestForm extends React.Component {
 }
 
 export default reduxForm({
-    form: 'return'
+    form: 'return',
+    onSubmitFail: (errors, dispatch) =>
+    dispatch(focus('return', Object.keys(errors)[0]))
 })(TestForm);
 
